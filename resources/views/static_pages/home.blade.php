2d9a5fc0022637904040719be274568cd55fddca
vagrant <vagrant@homestead>
2022-01-14 19:09:48 +0000

    name route

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
         <h1>Hello ZHEN!</h1>
         <p class="lead">你现在所看到的是<strong>ZHEN</strong></p>
         <p>一切，将从这里开始。</p>
-        <p>你可以从这里得到<a href="/help">帮助</a></p>
+        <p>你可以从这里得到<a href="{{route('help')}}">帮助</a></p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">现在注册</a></p>
     </div>
 @stop
