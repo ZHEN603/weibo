e2508ef2c59cea8d6866af3bdb2dedb848220509
vagrant <vagrant@homestead>
2022-04-06 21:25:00 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,7 +24,16 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $msg="欢迎您的加入,".$user->name;
+        session()->flash('success', $msg);
+        return redirect()->route('users.show',[$user]);
     }
 
 }
